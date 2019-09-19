8f25fadeb02bd8c037727ccce4af211adff2caf6
DannyLine <danny.lien1109@gmail.com>
2019-09-19 19:30:43 +0800

    initial commit Guess

---
+++ app/src/main/java/com/danny/guess/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.danny.guess
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+    fun check(view: View) {
+
+        val n = number.text.toString().toInt()
+        println("number : ${n}")
+
+    }
+
+
+
+}
